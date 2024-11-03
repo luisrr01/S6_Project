24aaa963a234e060e590e9b5ce40c4a224edfe82
luisrr01 <luisrosarodriguez01@gmail.com>
2024-11-03 14:30:26 -0500

    Gráficos exitosamente creados y listos para ser enviados

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # S6_Project
 El proyecto del sprint 6 del bootcamp de DS de Tripleten.
+
+Este proyecto incluye una aplicación web donde se puede mostrar un histograma y un gráfico de dispersión interactivos donde se visualiza la forma de los datos y la relación con las demás variables de la columna "odómeter".
---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+# agregar un encabezado
+st.header('Anuncios de ventas de coches')
+
+build_histogram = st.checkbox('Construir un histograma') # crear un checkbox
+build_dispersion = st.checkbox('Construir un gráfico de dispersión') # crear un checkbox
+
+if build_histogram: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para la columna odómetro para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_dispersion: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para la columna odómetro para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
